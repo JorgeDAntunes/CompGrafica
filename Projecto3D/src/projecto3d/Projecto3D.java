9345cb9d8ca20a9b3b7591713a806c404ac7eaed
JorgeDAntunes <jdias.antunes.91@gmail.com>
2013-09-06 15:56:31 +0100

    3D v3
    margens, rotação behavior, zoom behavior, transação behavior,
    ambientLight e PointLight

--- Projecto3D/src/projecto3d/Projecto3D.java
+++ Projecto3D/src/projecto3d/Projecto3D.java
@@ -6,12 +6,18 @@ package projecto3d;
 
 import com.sun.j3d.utils.applet.MainFrame;
 import com.sun.j3d.utils.behaviors.mouse.MouseRotate;
+import com.sun.j3d.utils.behaviors.mouse.MouseTranslate;
+import com.sun.j3d.utils.behaviors.mouse.MouseZoom;
 import com.sun.j3d.utils.geometry.Box;
+import com.sun.j3d.utils.geometry.Sphere;
 import com.sun.j3d.utils.universe.SimpleUniverse;
+import com.sun.org.apache.bcel.internal.classfile.Code;
 import java.applet.Applet;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GraphicsConfiguration;
 import javax.media.j3d.Alpha;
+import javax.media.j3d.AmbientLight;
 import javax.media.j3d.Appearance;
 import javax.media.j3d.Background;
 import javax.media.j3d.BoundingSphere;
@@ -19,11 +25,13 @@ import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
 import javax.media.j3d.ColoringAttributes;
 import javax.media.j3d.Material;
+import javax.media.j3d.PointLight;
 import javax.media.j3d.RotationInterpolator;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
 import javax.vecmath.Color3f;
 import javax.vecmath.Point3d;
+import javax.vecmath.Point3f;
 import javax.vecmath.Vector3f;
 
 /**
@@ -56,6 +64,7 @@ public class Projecto3D extends Applet{
     }
     private BranchGroup createSceneGraph() {
         BranchGroup root = new BranchGroup();
+        BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 100.0);
         TransformGroup tg = new TransformGroup();
         tg.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
         tg.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
@@ -93,22 +102,33 @@ public class Projecto3D extends Applet{
         orangeApp.setColoringAttributes(orangeCA);
         
         //margem cima
-        Box margemCima = new Box(3.5f, 0.1f,0.2f, medBlueApp);
+        Box margemCima = new Box(3.5f, 0.1f,0.5f, orangeApp);
 //        margemCima.setUserData(new String("Margem de Cima"));
         Transform3D trMC = new Transform3D();
-        trMC.setTranslation(new Vector3f(0f,5.1f,0.1f));
+        trMC.setTranslation(new Vector3f(0f,5.1f,0.5f));
         TransformGroup tgMC = new TransformGroup(trMC);
         tgMC.addChild(margemCima);
         
         //margem baixo
-        
+        Box margemBaixo = new Box(3.5f, 0.1f,0.5f, orangeApp);
+        Transform3D trMB = new Transform3D();
+        trMB.setTranslation(new Vector3f(0f,-5.1f,0.5f));
+        TransformGroup tgMB = new TransformGroup(trMB);
+        tgMB.addChild(margemBaixo);
         
         //margem direita
-        
+        Box margemDireita = new Box(0.3f, 5.2f,0.5f, orangeApp);
+        Transform3D trMD = new Transform3D();
+        trMD.setTranslation(new Vector3f(-3.3f,0f,0.5f));
+        TransformGroup tgMD = new TransformGroup(trMD);
+        tgMD.addChild(margemDireita);
         
         //margem esquerda
-        
-        
+        Box margemEsquerda = new Box(0.3f, 5.2f,0.5f, orangeApp);
+        Transform3D trME = new Transform3D();
+        trME.setTranslation(new Vector3f(3.3f,0f,0.5f));
+        TransformGroup tgME = new TransformGroup(trME);
+        tgME.addChild(margemEsquerda);
         
         
         //tampo
@@ -118,28 +138,78 @@ public class Projecto3D extends Applet{
         TransformGroup tgTampo = new TransformGroup(trTampo);
         tgTampo.addChild(tampo);
         tgTampo.addChild(tgMC);
+        tgTampo.addChild(tgMB);
+        tgTampo.addChild(tgMD);
+        tgTampo.addChild(tgME);
         tg.addChild(tgTampo);
         
+        
         //Rotação
-        Alpha alpha = new Alpha(-1, 4000);
-        RotationInterpolator rodar = new RotationInterpolator(alpha, tg);
+//        Alpha alpha = new Alpha(-1, 4000);
+//        RotationInterpolator rodar = new RotationInterpolator(alpha, tg);
 
-        BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 100.0);
-        rodar.setSchedulingBounds(bounds);
-        tg.addChild(rodar);
+//        rodar.setSchedulingBounds(bounds);
+//        tg.addChild(rodar);
         
 //        //Fundo
 //        Background bb = new Background(0f, 1f, 1f);
 //        bb.setApplicationBounds(bounds);
 //        root.addChild(bb);
         
-//        //criar rotação behavior
-//        MouseRotate behavior = new MouseRotate();
-//        behavior.setTransformGroup(tg);
-//        root.addChild(behavior);
-//        behavior.setSchedulingBounds(bounds);
+        //criar rotação behavior (botão esquerdo do rato)
+        MouseRotate behavior = new MouseRotate();
+        behavior.setTransformGroup(tg);
+        root.addChild(behavior);
+        behavior.setSchedulingBounds(bounds);
         
+        
+        // criar zoom behavior (scroll do rato)
+        MouseZoom behavior2 = new MouseZoom();
+        behavior2.setTransformGroup(tg);
+        root.addChild(behavior2);
+        behavior2.setSchedulingBounds(bounds);
+ 
+        // Criar transação behavior (botão direito do rato)
+        MouseTranslate behavior3 = new MouseTranslate();
+        behavior3.setTransformGroup(tg);
+        root.addChild(behavior3);
+        behavior3.setSchedulingBounds(bounds);
+ 
+        //luz Azul
+        Appearance lAzul = new Appearance();
+        lAzul.setCapability(Appearance.ALLOW_COLORING_ATTRIBUTES_WRITE);
+        Color3f cLAzul = new Color3f(0.5f, 1.0f, 1.0f);
+        ColoringAttributes caLAzul = new ColoringAttributes();
+        caLAzul.setColor(cLAzul);
+        lAzul.setColoringAttributes(caLAzul);
+        
+        //Criar esfera
+        Sphere bola = new Sphere(0.25f, blueApp);
+        Transform3D trBola = new Transform3D();
+        trBola.setTranslation(new Vector3f(3.3f,0f,0.5f));
+        TransformGroup tgBola = new TransformGroup(trBola);
+        tgBola.addChild(bola);
 //        root.addChild(tgTampo);
+        
+        
+        
+        //Luz
+        AmbientLight luz = new AmbientLight(true, new Color3f(Color.WHITE));
+        luz.setInfluencingBounds(bounds);
+        root.addChild(luz);
+        
+        //cor
+        PointLight ptlight = new PointLight(new Color3f(Color.YELLOW),
+                new Point3f(0f, 0f, 2f), new Point3f(1f, 0f, 0f));
+        ptlight.setInfluencingBounds(bounds);
+        root.addChild(ptlight);
+        PointLight ptlight2 = new PointLight(new Color3f(Color.YELLOW),
+                new Point3f(-2f, 2f, 2f), new Point3f(1f, 0f, 0f));
+        ptlight2.setInfluencingBounds(bounds);
+        root.addChild(ptlight2);
+        
+        
+        
         
         return root;
         
